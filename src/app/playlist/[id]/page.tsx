4b4e3eae7a31ecf4828cd7aa6f39df15f7ef1247
steffancross <steffancross@berkeley.edu>
2025-08-30 22:09:31 -0700

    little more styling for playlist

--- src/app/playlist/[id]/page.tsx
+++ src/app/playlist/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { api } from "~/trpc/server";
+import { Ellipsis } from "lucide-react";
 
 const Playlist = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id: playlistId } = await params;
@@ -8,21 +9,32 @@ const Playlist = async ({ params }: { params: Promise<{ id: string }> }) => {
   const songs = playlist?.playlistEntries;
 
   return (
-    <>
-      <h1>{playlist?.name}</h1>
-      {songs?.map((song) => {
+    <div className="grid w-full place-items-center">
+      <h1 className="mt-7">{playlist?.name}</h1>
+      {songs?.map((song, index) => {
         return (
-          <div key={song.position} className="flex flex-row justify-between">
-            <p>{song.libraryTrack.title}</p>
-            <p>
-              {song.libraryTrack.artists
-                .map((artist) => artist.artist.name)
-                .join(", ")}
-            </p>
+          <div
+            key={song.position}
+            className="flex w-7/10 flex-row items-center justify-between border border-blue-500"
+          >
+            <div className="flex flex-row items-center gap-4">
+              <p>{index + 1}.</p>
+              <div className="flex flex-col">
+                <p>{song.libraryTrack.title}</p>
+                <p>
+                  {song.libraryTrack.artists
+                    .map((artist) => artist.artist.name)
+                    .join(", ")}
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-row">
+              <Ellipsis />
+            </div>
           </div>
         );
       })}
-    </>
+    </div>
   );
 };
 
