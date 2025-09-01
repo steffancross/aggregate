fb389c4b0e3f2cf620a07a76a3d4a6670c48c591
steffancross <steffancross@berkeley.edu>
2025-09-01 16:21:47 -0700

    playlist item component

---
+++ src/app/playlist/[id]/PlaylistItem.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useState } from "react";
+import { Ellipsis, Play } from "lucide-react";
+
+interface PlaylistItemProps {
+  index: number;
+  title: string;
+  position: number;
+  artists: { artist: { name: string } }[];
+}
+
+export const PlaylistItem = ({
+  index,
+  title,
+  position,
+  artists,
+}: PlaylistItemProps) => {
+  const [hovered, setHovered] = useState(false);
+
+  return (
+    <div
+      key={position}
+      className="flex w-7/10 flex-row items-center justify-between border border-blue-500"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <div className="flex flex-row items-center gap-4">
+        {hovered ? <Play /> : <p>{index + 1}</p>}
+        <div className="flex flex-col">
+          <p>{title}</p>
+          <p>{artists.map((artist) => artist.artist.name).join(", ")}</p>
+        </div>
+      </div>
+      <div className="flex flex-row">
+        <Ellipsis />
+      </div>
+    </div>
+  );
+};
