37f22ea0d1d6b730df68849726c893daa9610a75
steffancross <steffancross@berkeley.edu>
2025-11-16 17:46:41 -0800

    adding a youtube track

--- src/lib/actions/getTrackData.ts
+++ src/lib/actions/getTrackData.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import type { SongSource } from "@prisma/client";
+import { Temporal } from "@js-temporal/polyfill";
 
 interface SoundCloudResolveResponse {
   title: string;
@@ -99,15 +100,73 @@ async function fetchSoundCloudMetadata(url: string) {
   };
 }
 
+interface YoutubeResponse {
+  items: {
+    id: string;
+    snippet: {
+      title: string;
+      thumbnails: {
+        default: {
+          url: string;
+        };
+        standard: {
+          url: string;
+        };
+        maxres: {
+          url: string;
+        };
+      };
+      channelTitle: string;
+    };
+    contentDetails: {
+      duration: string;
+    };
+  }[];
+}
+
+const getYoutubeId = (url: string) => {
+  const parsed = new URL(url);
+  return parsed.searchParams.get("v");
+};
+
 async function fetchYouTubeMetadata(url: string) {
+  const youtubeId = getYoutubeId(url);
+  const apiKey = process.env.YT_API_KEY;
+
+  const youtubeUrl = `https://www.googleapis.com/youtube/v3/videos?id=${youtubeId}&key=${apiKey}&part=snippet,contentDetails`;
+
+  const res = await fetch(youtubeUrl);
+
+  if (!res.ok) {
+    const errorText = await res.text();
+    console.error("YouTube API error:", errorText);
+    throw new Error(
+      `YouTube API error: ${res.status} ${res.statusText} - ${errorText}`,
+    );
+  }
+
+  const data = (await res.json()) as YoutubeResponse;
+  const videoData = data.items[0];
+
+  if (!data.items || data.items.length === 0 || !videoData) {
+    throw new Error(`No video found for YouTube ID: ${youtubeId}`);
+  }
+
+  const duration = Temporal.Duration.from(
+    videoData.contentDetails.duration,
+  ).total({ unit: "millisecond" });
+
   return {
-    title: "",
-    artist: [],
+    title: videoData.snippet.title,
+    artist: [videoData.snippet.channelTitle],
     album: "",
-    duration: 0,
-    artworkUrl: "",
+    duration: duration,
+    artworkUrl:
+      videoData.snippet.thumbnails.maxres.url ||
+      videoData.snippet.thumbnails.standard.url ||
+      "",
     sourceUrl: url,
-    sourceId: "",
+    sourceId: videoData.id,
     source: "youtube" as const,
   };
 }
